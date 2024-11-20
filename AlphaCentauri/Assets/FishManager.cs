e1b1969178eecfc977d488aa63791a698b5203ff
snailsquid <hexagononyt@gmail.com>
2024-11-21 04:35:39 +0700

    refactor - central time manager and bucket implementation

---
+++ AlphaCentauri/Assets/FishManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FishManager : MonoBehaviour
+{
+    Bucket currentBucket;
+    void Start()
+    {
+        currentBucket = BucketRegistry.Buckets[BucketRegistry.Tier.Basic];
+    }
+}
