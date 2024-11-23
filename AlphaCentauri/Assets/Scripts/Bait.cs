5ab01b6a1708cb9d299eaabe3f1b7acb35ff9b94
Knightio29 <akmalssatrio@gmail.com>
2024-11-23 09:18:20 +0700

    datafilling (1)

--- AlphaCentauri/Assets/Scripts/Bait.cs
+++ AlphaCentauri/Assets/Scripts/Bait.cs
@@ -71,16 +71,69 @@ public class BaitRegistry
     }
     public static Dictionary<BaitType, Bait> Baits { get; private set; } = new Dictionary<BaitType, Bait>(){
         {BaitType.None,new Bait("None", new Dictionary<FishTemplate, float>(){
-            {FishTemplates[FishType.Bass], 0},
-            {FishTemplates[FishType.Salmon],20}
+            {FishTemplates[FishType.Ampas], 55},
+            {FishTemplates[FishType.Tuna], 10},
+            {FishTemplates[FishType.Carp], 10},
+            {FishTemplates[FishType.Catfish], 10},
+            {FishTemplates[FishType.Special], 5},
+            {FishTemplates[FishType.Salmon],10},
         })},
-        {BaitType.CacingTanah,new Bait("wormy", new Dictionary<FishTemplate, float>(){
-            {FishTemplates[FishType.Bass], 0},
-            {FishTemplates[FishType.Salmon],20}
+        {BaitType.Pellet,new Bait("Pellet", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.Carp], 20},
+            {FishTemplates[FishType.Trout], 35},
+            {FishTemplates[FishType.Special], 5},
+            {FishTemplates[FishType.Ampas], 5},
+            {FishTemplates[FishType.Salmon],35},
         })},
-        {BaitType.Mackarel,new Bait("MACKAREL OMG", new Dictionary<FishTemplate, float>(){
-            {FishTemplates[FishType.Bass], 0},
-            {FishTemplates[FishType.Salmon],20}
+        {BaitType.CacingTanah,new Bait("CacingTanah", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.Bass], 40},
+            {FishTemplates[FishType.Catfish],30},
+            {FishTemplates[FishType.Tuna],20},
+            {FishTemplates[FishType.Special],5},
+            {FishTemplates[FishType.Ampas],5},
+        })},
+        {BaitType.Jangkrik,new Bait("Jangkrik", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.KingCrab], 35},
+            {FishTemplates[FishType.Eel], 35},
+            {FishTemplates[FishType.Trout], 10},
+            {FishTemplates[FishType.Carp], 10},
+            {FishTemplates[FishType.Special],5},
+            {FishTemplates[FishType.Ampas],5},
+        })},
+        {BaitType.DagingCincang,new Bait("DagingCincang", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.Pufferfish], 35},
+            {FishTemplates[FishType.Sailfish],35},
+            {FishTemplates[FishType.Salmon],10},
+            {FishTemplates[FishType.Bass],10},
+            {FishTemplates[FishType.Special],5},
+            {FishTemplates[FishType.Ampas],5},
+        })},
+        {BaitType.BeefWellington,new Bait("BeefWellington", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.GreatWhiteShark], 5},
+            {FishTemplates[FishType.Sailfish], 45},
+            {FishTemplates[FishType.GiantSquid], 10},
+            {FishTemplates[FishType.MarianaCrab], 10},
+            {FishTemplates[FishType.Salmon], 10},
+            {FishTemplates[FishType.Trout], 5},
+            {FishTemplates[FishType.Special], 10},
+            {FishTemplates[FishType.Ampas], 5},
+        })},
+        {BaitType.Mackarel,new Bait("Mackarel", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.GiantSquid], 50},
+            {FishTemplates[FishType.MantaRay], 20},
+            {FishTemplates[FishType.KingCrab], 10},
+            {FishTemplates[FishType.Eel], 10},
+            {FishTemplates[FishType.Special], 5},
+            {FishTemplates[FishType.Ampas], 5},
+        })},
+        {BaitType.MarianaCrab,new Bait("MarianaCrab", new Dictionary<FishTemplate, float>(){
+            {FishTemplates[FishType.MantaRay], 40},
+            {FishTemplates[FishType.GiantSquid], 15},
+            {FishTemplates[FishType.Sailfish], 15},
+            {FishTemplates[FishType.Pufferfish], 10},
+            {FishTemplates[FishType.Catfish], 10},
+            {FishTemplates[FishType.Special], 5},
+            {FishTemplates[FishType.Ampas], 5},
         })},
     };
     public BaitRegistry()
